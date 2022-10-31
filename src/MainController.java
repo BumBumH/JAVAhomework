2a51eb15e5525545715841fb3bf09bb9a0e902f9
BumBumH <BumBumH@DESKTOP-S2M9KE2>
2022-11-01 08:56:20 +0900

    This is JAVA sample project

---
+++ src/MainController.java
@@ -0,0 +1,70 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+
+public class MainController implements ActionListener{
+  
+  private final String CODE_ROCK = "바위";
+  private final String CODE_SCISSORS = "가위";
+  private final String CODE_PAPER = "보";
+
+  private MainFrame mainFrame = new MainFrame();
+
+  public MainController(){
+    //가위 바위 보 버튼 리스너를 설정 
+    mainFrame.getButtonRock().addActionListener(this);
+    mainFrame.getButtonScissors().addActionListener(this);
+    mainFrame.getButtonPaper().addActionListener(this);
+    // 잠만 getButtonSelect().addActionListener(this);
+  }
+  
+  //승리 패턴을 문자열 화 해두고 "내가 내 는 것":"상대가 낸 것"
+  private List<String> victoryList;
+  {
+    this.victoryList = new ArrayList<String>();
+    this.victoryList.add(this.CODE_ROCK + ":" + this.CODE_SCISSORS);
+    this.victoryList.add(this.CODE_SCISSORS + ":" + this.CODE_PAPER);
+    this.victoryList.add(this.CODE_PAPER + ":" + this.CODE_ROCK);
+  }
+  
+  //눌린 버튼과 작동 버튼의 매칭화 
+  private Map<Object, String> rpsMap; 
+  {
+    //오브잭트의 등가로 판단했기 때문에 IdentityHashMap을 사용
+    this.rpsMap = new IdentityHashMap<Object, String>();
+    this.rpsMap.put(this.mainFrame.getButtonRock(),this.CODE_ROCK);
+    this.rpsMap.put(this.mainFrame.getButtonScissors(),this.CODE_SCISSORS);
+    this.rpsMap.put(this.mainFrame.getButtonPaper(),this.CODE_PAPER);
+
+  }
+  
+  /** 
+   * 가위바위보 버튼 입력 시 액션 리스너 
+   */
+  @Override
+  public void actionPerformed(ActionEvent e){
+    //눌린 버튼으로 코드치를 추출 
+    String result = this.rpsMap.get(e.getSource());
+    
+    //컴퓨터의 선택을 랜덤으로 설정
+    Random random = new Random();
+    String cpuResult = new String[]{this.CODE_ROCK,this.CODE_SCISSORS,this.CODE_PAPER}[random.nextInt(3)];
+    
+    //비김 
+    if(result.equals(cpuResult)){
+      this.mainFrame.setSubTitle("저는" + cpuResult + "를 냈습니다. 비겼습니다.");
+    //승리
+    }else if(this.victoryList.contains(result + ":" + cpuResult)){
+      this.mainFrame.setSubTitle("저는" + cpuResult + "를 냈습니다. 당신의 승리입니다.");
+    //패배
+    }else{
+      this.mainFrame.setSubTitle("저는" + cpuResult + "를 냈습니다. 당신의 패배입니다.");
+      
+    }
+  }
+}
---
+++ src/MainFrame.java
@@ -0,0 +1,93 @@
+import java.awt.Image;
+//오류import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame{
+  
+
+  //배경이미지 (에러)
+  //private Image background=new ImageIcon(MainController.class.getResource("../image/stage.png")).getImage();
+  //바위버튼
+  private JButton buttonRock;
+  
+  //가위버튼
+  private JButton buttonScissors;
+  
+  //보버튼
+  private JButton buttonPaper;
+  
+  //코인 버튼 (에러)
+  //private JButton buttonCoin;
+  
+  //서브타이틀
+  private JLabel labelSubTitle = new JLabel("버튼을 눌러주세요");
+  
+  public MainFrame(){
+    super("가위바위보 게임");
+    
+    super.setBounds(500,500,800,800);
+    
+    super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    JPanel panel = new JPanel();
+
+    //JButton의 사진을 리사이즈 하기 위해선 ImageIcon작성⇒Image오브젝트 추출하여 재지정 ⇒ImageIcon으로 재 설정
+    ImageIcon imageRock = new ImageIcon("./button_Image/rock.png");
+    imageRock.setImage(imageRock.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
+    this.buttonRock = new JButton(imageRock);
+    
+    ImageIcon imageScissors = new ImageIcon("./button_Image/sc.png");
+    imageScissors.setImage(imageScissors.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
+    this.buttonScissors = new JButton(imageScissors);
+    
+    ImageIcon imagePaper = new ImageIcon("./button_Image/paper.png");
+    imagePaper.setImage(imagePaper.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
+    this.buttonPaper = new JButton(imagePaper);
+    
+    //ImageIcon imageCoin = new ImageIcon("./button_Image/coin.png");
+    imagePaper.setImage(imagePaper.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
+    this.buttonPaper = new JButton(imagePaper);
+    
+    super.add("North",labelSubTitle);
+    
+    panel.add(buttonRock);
+    panel.add(buttonScissors);
+    panel.add(buttonPaper);
+    labelSubTitle.add(panel);
+   //에러 panel.add(buttonCpu);
+    super.add("South", panel);
+    
+    super.setVisible(true);
+    
+  }
+  
+  /**
+   * 서브타이틀 설정.
+   * @param subTitle
+   */
+  public void setSubTitle(String subTitle){
+    labelSubTitle.setText(subTitle);
+  }
+
+  public JButton getButtonRock() {
+    return buttonRock;
+  }
+
+  public JButton getButtonScissors() {
+    return buttonScissors;
+  }
+
+  public JButton getButtonPaper() {
+    return buttonPaper;
+  }
+  //public JButton getButtonCoin() {
+	   // return buttonCoin;
+	  }
+  
+
